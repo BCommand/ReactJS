0e74f72d8670efc6ce71e3c58e0587c6702949ec
bcommand <brisa_com_12@hotmail.com>
2022-01-29 18:23:48 -0300

    Nav Bar

---
+++ Desktop/ReactJS/vivirhoy/src/Components/NavBar.js
@@ -0,0 +1,29 @@
+import "./NavBar.css";
+
+import CardWidget from "./CardWidget.js";
+
+const NavBar = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <nav
+        className="Navigation"
+        style={{ border: `10px solid ${props.myColor}` }}
+      >
+        <ul>
+          <li>
+            <CardWidget />
+          </li>
+          <li>Home</li>
+          <li>Galeria</li>
+          <li>Info</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
+
