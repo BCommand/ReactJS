d5f0b2c61e2e56e69904bd2f7b15b3766540bd66
bcommand <brisa_com_12@hotmail.com>
2022-01-30 16:21:00 -0300

    test

--- Desktop/ReactJS/vivirhoy/src/App.js
+++ Desktop/ReactJS/vivirhoy/src/App.js
@@ -1,22 +1,42 @@
-import "./styles.css";
+import React, { Component } from 'react';
+import './App.css';
 
-import NavBar from "./Components/NavBar";
-
-const name = "Daniel";
-
-const brownColor = "brown";
+// COMPONENTS
+import CardComponent from './Components/CardComponent/CardComponent';
+import Header from './Components/Header/Header';
 
 
-const App = () => {
-  return (
-    <>
-      <NavBar myColor={brownColor} />
-
-   
-    </>
-  );
-};
+class App extends Component {
+	render() {
+		return (
+			<div className='App'>
+				<Header
+					title='Soy el titulo a partir de una prop'
+					subtitle='Vivir Hoy'
+				/>
+				<div className='UserSection'>
+					<CardComponent
+						name='Daniel Di Salvo'
+						date='Se unio en Enero 2021'
+						description='Front-end Developer y profesor del Curso de React.js en CoderHouse'
+						img='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+					/>
+					<CardComponent
+						name='Raul Gutierrez'
+						date='Se unió en Abril 2021'
+						description='Scrum Master y UX Designer en Mercado Libre'
+						img='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+					/>
+					<CardComponent
+						name='Francesca Bertotti'
+						date='Se unió en Julio 2021'
+						description='DevOps y profesora del curso de React.js en CoderHouse'
+						img='https://react.semantic-ui.com/images/avatar/large/daniel.jpg'
+					/>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default App;
-
-
